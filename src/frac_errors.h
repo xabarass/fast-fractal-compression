8a4ec96cf01265f52575e331ca7386943873fc03
Anton Permenev <toshnh@users.noreply.github.com>
2017-03-22 20:58:01 +0100

    added error code, so Manuel's code compiles

--- src/frac_errors.h
+++ src/frac_errors.h
@@ -9,7 +9,7 @@ typedef enum _ERROR_CODES{
     ERR_ACCUM_ROLL, //Accumulator rolled over averaging pixels
     ERR_OVERFLOW,
     ERR_IMAGE_WRONG_SIZE,   //Image must have dimensions that are multiples of 32
-
+    ERR_NO_IMAGE_PATH
 } ERR_RET;
 
 #endif // FRAC_ERRORS_H
