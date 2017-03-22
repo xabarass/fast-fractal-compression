8f6f870e479a6153bba898d7ad6c806ec3ea9fbd
Manuel Rodríguez <manuel.rodriguezgonzalo@gmail.com>
2017-03-22 18:35:56 +0100

    Added command line arguments, so that Anton does not hardcode his local paths and causes segfaults :P

--- src/FractalCompression.cpp
+++ src/FractalCompression.cpp
@@ -2,14 +2,21 @@
 #include <GitVersion.h>
 #include "emmintrin.h"
 #include "include/perf.h"
-
+#include <sys/stat.h>
+#include <string>
+#include <unistd.h>
 #include <utils/BMPImage.h>
 #include <encoder/Encoder.h>
 #include <decoder/Decoder.h>
 
 using namespace std;
 
-int main(){
+inline bool exists_image(const std::string& name) {
+  struct stat buffer;   
+  return (stat (name.c_str(), &buffer) == 0); 
+}
+
+int main(int argc, char** argv){
     cout<<"    ______           __        ______                __        __   "<<endl;
     cout<<"   / ____/___ ______/ /_      / ____/________ ______/ /_____ _/ /   "<<endl;
     cout<<"  / /_  / __ `/ ___/ __/_____/ /_  / ___/ __ `/ ___/ __/ __ `/ /    "<<endl;
@@ -19,10 +26,22 @@ int main(){
     cout<<"Git version: "<<" "<<s_GIT_SHA1_HASH<<" "<<s_GIT_REFSPEC<<endl;
     cout<<endl;
 
+    if (argc != 2) {
+        std::cout << "Usage: fractal-compression <path-to-image>\n";
+        return ERR_NO_IMAGE_PATH;
+    }
+    std::string image_path(argv[1]);
+
+    if (!exists_image(image_path.c_str())) {
+        std::cout << "Provided image does not exist\n";
+        return ERR_NO_IMAGE_PATH;
+    }
+
+
     perf_init();
 
     cycles_count_start ();
-    BMPImage img("/Users/toshnh/study/htwfnc/project/fractal-compression/test_images/lena.bmp");
+    BMPImage img(image_path.c_str());
     img.Load();
     BMPImage res("result.bmp", img.GetWidth(), img.GetHeight(), img.GetChannels());
     Encoder enc;
