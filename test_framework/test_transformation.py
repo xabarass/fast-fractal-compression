fb687a516d52f4e4e49496004e00665ea4764f7b
Jinank Jain <jinank94@gmail.com>
2017-05-05 14:08:09 +0200

    Updated test_transformation.py

--- test_framework/test_transformation.py
+++ test_framework/test_transformation.py
@@ -11,27 +11,23 @@ THRESHOLD = 100
 PHASES = 5
 
 
-def generate_reference():
+def generate_reference(name):
 	LOG_FOLDER = "test_framework"
 	LOG_SUB_FOLDER = "standards"
 	OUTPUT_FOLDER = "test_framework"
 	OUTPUT_SUB_FOLDER = "reference_images"
-	for image in test_images:
-		image_path = os.path.join(TEST_IMAGE_FOLDER, TEST_IMAGE_SUB_FOLDER, "{}.bmp".format(image))
-		log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(image))
-		output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_SUB_FOLDER, "{}.bmp".format(image))
-		command = "-t {} -p {} -o {} -f {} {}".format(THRESHOLD, PHASES, output_path, log_path, image_path)
-		return command
+	log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(name))
+	output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_SUB_FOLDER, "{}.bmp".format(name))
+	command = "-t {} -p {} -o {} -f {} {}".format(THRESHOLD, PHASES, output_path, log_path)
+	return command
 
 
-def run_test():
+def run_test(name):
 	LOG_FOLDER = "test_framework"
 	LOG_SUB_FOLDER = "transformation_log"
 	OUTPUT_FOLDER = "test_framework"
 	OUTPUT_SUB_FOLDER = "image_log"
-	for image in test_images:
-		image_path = os.path.join(TEST_IMAGE_FOLDER, TEST_IMAGE_SUB_FOLDER, "{}.bmp".format(image))
-		log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(image))
-		output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_SUB_FOLDER, "{}.bmp".format(image))
-		command = "-t {} -p {} -o {} -f {} {}".format(THRESHOLD, PHASES, output_path, log_path, image_path)
-		return command
+	log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(name))
+	output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_SUB_FOLDER, "{}.bmp".format(name))
+	command = "-t {} -p {} -o {} -f {}".format(THRESHOLD, PHASES, output_path, log_path)
+	return command
