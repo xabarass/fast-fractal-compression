794f411b9a324ffa40c4777b4b3a68e61966daca
Jinank Jain <jinank94@gmail.com>
2017-05-05 14:19:14 +0200

    compare_transformation_diff

--- test_framework/test_transformation.py
+++ test_framework/test_transformation.py
@@ -10,24 +10,31 @@ TEST_IMAGE_SUB_FOLDER = "benchmark"
 THRESHOLD = 100
 PHASES = 5
 
+LOG_FOLDER = "test_framework"
+LOG_SUB_FOLDER = "transformation_log"
+LOG_REF_FOLDER = "standards"
+OUTPUT_FOLDER = "test_framework"
+OUTPUT_SUB_FOLDER = "image_log"
+OUTPUT_REF_FOLDER = "reference_images"
 
-def generate_reference(name):
-	LOG_FOLDER = "test_framework"
-	LOG_SUB_FOLDER = "standards"
-	OUTPUT_FOLDER = "test_framework"
-	OUTPUT_SUB_FOLDER = "reference_images"
-	log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(name))
-	output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_SUB_FOLDER, "{}.bmp".format(name))
+
+def generate_reference(name, image_name):
+	log_path = os.path.join(LOG_FOLDER, LOG_REF_FOLDER, "{}.txt".format(name+image_name))
+	output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_REF_FOLDER, name+image_name)
 	command = "-t {} -p {} -o {} -f {} {}".format(THRESHOLD, PHASES, output_path, log_path)
 	return command
 
 
 def run_test(name):
-	LOG_FOLDER = "test_framework"
-	LOG_SUB_FOLDER = "transformation_log"
-	OUTPUT_FOLDER = "test_framework"
-	OUTPUT_SUB_FOLDER = "image_log"
-	log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(name))
-	output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_SUB_FOLDER, "{}.bmp".format(name))
+	log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(name+image_name))
+	output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_SUB_FOLDER, name+image_name)
 	command = "-t {} -p {} -o {} -f {}".format(THRESHOLD, PHASES, output_path, log_path)
 	return command
+
+def compare_transformation_diff(name, image_name):
+	ref_path = os.path.join(LOG_FOLDER, LOG_REF_FOLDER, "{}.txt".format(name+image_name))
+	log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(name+image_name))
+	command = "diff -u {} {}".format(ref_path, log_path)
+	output = subprocess.checkout(command.split(' '))
+	result = output.decode("utf-8")
+	print(result)
