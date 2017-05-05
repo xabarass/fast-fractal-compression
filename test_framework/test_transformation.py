edde76dbb036cab3d048a07c7056f11e6a2766a6
Jinank Jain <jinank94@gmail.com>
2017-05-05 13:39:50 +0200

    Test Framework

--- test_framework/test_transformation.py
+++ test_framework/test_transformation.py
@@ -1,4 +1,6 @@
 import os
+import time
+from git import Repo
 
 os.chdir("..")
 
@@ -7,14 +9,29 @@ TEST_IMAGE_FOLDER = "test_images"
 TEST_IMAGE_SUB_FOLDER = "benchmark"
 THRESHOLD = 100
 PHASES = 5
-LOG_FOLDER = "test_framework"
-LOG_SUB_FOLDER = "standards"
-OUTPUT_FOLDER = "test_framework"
-OUTPUT_SUB_FOLDER = "reference_images"
 
-for image in test_images:
-	image_path = os.path.join(TEST_IMAGE_FOLDER, TEST_IMAGE_SUB_FOLDER, "{}.bmp".format(image))
-	log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(image))
-	output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_SUB_FOLDER, "{}.bmp".format(image))
-	command = "./bin/fractal-compression -t {} -p {} -o {} -f {} {}".format(THRESHOLD, PHASES, output_path, log_path, image_path)
-	os.system(command)
+
+def generate_reference():
+	LOG_FOLDER = "test_framework"
+	LOG_SUB_FOLDER = "standards"
+	OUTPUT_FOLDER = "test_framework"
+	OUTPUT_SUB_FOLDER = "reference_images"
+	for image in test_images:
+		image_path = os.path.join(TEST_IMAGE_FOLDER, TEST_IMAGE_SUB_FOLDER, "{}.bmp".format(image))
+		log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(image))
+		output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_SUB_FOLDER, "{}.bmp".format(image))
+		command = "-t {} -p {} -o {} -f {} {}".format(THRESHOLD, PHASES, output_path, log_path, image_path)
+		return command
+
+
+def run_test():
+	LOG_FOLDER = "test_framework"
+	LOG_SUB_FOLDER = "transformation_log"
+	OUTPUT_FOLDER = "test_framework"
+	OUTPUT_SUB_FOLDER = "image_log"
+	for image in test_images:
+		image_path = os.path.join(TEST_IMAGE_FOLDER, TEST_IMAGE_SUB_FOLDER, "{}.bmp".format(image))
+		log_path = os.path.join(LOG_FOLDER, LOG_SUB_FOLDER, "{}.txt".format(image))
+		output_path = os.path.join(OUTPUT_FOLDER, OUTPUT_SUB_FOLDER, "{}.bmp".format(image))
+		command = "-t {} -p {} -o {} -f {} {}".format(THRESHOLD, PHASES, output_path, log_path, image_path)
+		return command
