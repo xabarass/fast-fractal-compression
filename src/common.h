b40a7fc41979d6c5113b93209a136cf950532a61
Jinank Jain <jinank94@gmail.com>
2017-05-11 22:00:52 +0200

    my bad

--- src/common.h
+++ src/common.h
@@ -7,8 +7,8 @@
 #include <stdint.h>
 #include <assert.h>
 
-typedef u_int8_t uint8_t;
-typedef u_int32_t uint32_t;
+typedef uint8_t u_int8_t;
+typedef uint32_t u_int32_t;
 
 #ifdef COUNT_FLOPS
     struct global_op_count{
